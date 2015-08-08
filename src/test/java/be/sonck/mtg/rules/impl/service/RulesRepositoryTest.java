82b256d3f147e42fdeedc7252529789fda55deb3
Johan Sonck <johan.sonck@gmail.com>
2015-08-09 00:10:38 +0200

    added some more RulesRepository tests

--- src/test/java/be/sonck/mtg/rules/impl/service/RulesRepositoryTest.java
+++ src/test/java/be/sonck/mtg/rules/impl/service/RulesRepositoryTest.java
@@ -24,17 +24,19 @@ public class RulesRepositoryTest {
 
     private static final String RULE_1 = "rule1";
     private static final String RULE_2 = "rule2";
-    private static final String RULE_3 = "rule3";
+    private static final String RULE_100 = "rule100";
+    private static final String RULE_100_1 = "rule100_1";
+    private static final String RULE_100_1A = "rule100_1a";
+    private static final String RULE_100_1B = "rule100_1b";
+    private static final String RULE_100_2 = "rule100_2";
+    private static final String RULE_101 = "rule101";
 
     @Mock
     private RulesParser rulesParser;
 
     @Test
     public void getRule() {
-        expectGetRules(new StringHashMapBuilder()
-                .entry("1.", RULE_1)
-                .entry("2.", RULE_2)
-                .build());
+        expectGetRules();
 
         validateRule(repository().getRule("1."), RULE_1);
         validateRule(repository().getRule("2."), RULE_2);
@@ -42,28 +44,62 @@ public class RulesRepositoryTest {
 
     @Test
     public void getRuleWithChildren() {
-        expectGetRules(new StringHashMapBuilder()
-                .entry("1.", RULE_1)
-                .entry("100.", RULE_2)
-                .entry("101.", RULE_3)
-                .build());
+        expectGetRules();
 
-        Optional<Rule> optionalRule = repository().getRule("1.");
-        validateRule(optionalRule, RULE_1);
-
-        Collection<Rule> children = optionalRule.get().getChildren();
+        Collection<Rule> children = repository().getRule("1.").get().getChildren();
         assertFalse(isEmpty(children));
         assertThat(children.size(), is(2));
 
         Iterator<Rule> ruleIterator = children.iterator();
 
-        Rule child = ruleIterator.next();
-        assertThat(child.getId(), is("100."));
-        assertThat(child.getText(), is(RULE_2));
+        validateRule(ruleIterator.next(), "100.", RULE_100);
+        validateRule(ruleIterator.next(), "101.", RULE_101);
+    }
 
-        child = ruleIterator.next();
-        assertThat(child.getId(), is("101."));
-        assertThat(child.getText(), is(RULE_3));
+    @Test
+    public void getRuleWithGrandChildren() {
+        expectGetRules();
+
+        Collection<Rule> grandChildren = repository().getRule("1.").get().getChildren().iterator().next().getChildren();
+
+        Iterator<Rule> ruleIterator = grandChildren.iterator();
+
+        validateRule(ruleIterator.next(), "100.1.", RULE_100_1);
+        validateRule(ruleIterator.next(), "100.2.", RULE_100_2);
+    }
+
+    @Test
+    public void getRuleWithGrandGrandChildren() {
+        expectGetRules();
+
+        Collection<Rule> grandGrandChildren = repository().getRule("1.").get().getChildren().iterator().next().getChildren().iterator().next().getChildren();
+
+        Iterator<Rule> ruleIterator = grandGrandChildren.iterator();
+
+        validateRule(ruleIterator.next(), "100.1a", RULE_100_1A);
+        validateRule(ruleIterator.next(), "100.1b", RULE_100_1B);
+    }
+
+    @Test
+    public void getDeepRule() {
+        expectGetRules();
+
+        validateRule(repository().getRule("100.1a"), RULE_100_1A);
+    }
+
+    private void expectGetRules() {
+        Map<String, String> rules = new StringHashMapBuilder()
+                .entry("1.", RULE_1)
+                .entry("100.", RULE_100)
+                .entry("100.1.", RULE_100_1)
+                .entry("100.1a", RULE_100_1A)
+                .entry("100.1b", RULE_100_1B)
+                .entry("100.2.", RULE_100_2)
+                .entry("101.", RULE_101)
+                .entry("2.", RULE_2)
+                .build();
+
+        expectGetRules(rules);
     }
 
     private void expectGetRules(Map<String, String> map) {
@@ -73,6 +109,12 @@ public class RulesRepositoryTest {
     private void validateRule(Optional<Rule> optionalRule, String ruleText) {
         assertTrue(optionalRule.isPresent());
         assertThat(optionalRule.get().getText(), is(ruleText));
+    }
+
+    private void validateRule(Rule rule, String ruleId, String ruleText) {
+        assertNotNull(rule);
+        assertThat(rule.getId(), is(ruleId));
+        assertThat(rule.getText(), is(ruleText));
     }
 
     private RulesRepository repository() {
